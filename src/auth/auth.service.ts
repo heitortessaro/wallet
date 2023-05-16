49806630935db3a912266e076619c3bb4b7eb1bf
Heitor <heitor.j.tessaro@gmail.com>
2023-05-15 21:41:53 -0300

    feat(Auth): define the signup service
    it uses argon2 to hash the password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) { }
-  signup() {
-    return { msg: 'signup' };
+
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          firstName: dto.fistName,
+          lastName: dto.lastName,
+          hash,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken. There is a unique constraint violation',
+          );
+        }
+      }
+    }
   }
 
   signin() {
