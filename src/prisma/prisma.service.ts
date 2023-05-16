1a12e9e8bfb527f761a21e2d43e99c44f208419e
Heitor <heitor.j.tessaro@gmail.com>
2023-05-16 19:08:15 -0300

    feat(Auth): use config to add the env variable on PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 // import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgreswallet:postgreswallet@localhost:5434/wallet?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
