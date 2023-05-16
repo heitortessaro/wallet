567734c7a70d440ac5ed38bd3d8cb545912c4aae
Heitor <heitor.j.tessaro@gmail.com>
2023-05-15 21:51:56 -0300

    feat(Auth): define the signin service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -33,7 +33,21 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { msg: 'signin' };
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // check if user exists
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    // send back the user
+    return user;
   }
 }
