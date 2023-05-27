1e62f3f8359a1892e148b1d2fdc7d25b38f50504
Heitor <heitor.j.tessaro@gmail.com>
2023-05-27 09:31:00 -0300

    feat(Auth): define a user module to test guards implementation
    guards use strategies to protect routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    PrismaModule,
+    UserModule,
+  ],
 })
 export class AppModule { }
