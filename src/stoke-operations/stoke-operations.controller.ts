28825691acfbfe28180b403b7407705df0e499cf
Heitor <heitor.j.tessaro@gmail.com>
2023-05-28 16:54:53 -0300

    feat(stoke-operations): define the routes for the stoke-operations controller

--- src/stoke-operations/stoke-operations.controller.ts
+++ src/stoke-operations/stoke-operations.controller.ts
@@ -1,4 +1,62 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Get,
+  Param,
+  Body,
+  Post,
+  Patch,
+  ParseIntPipe,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { StokeOperationsService } from './stoke-operations.service';
+import { GetUser } from '../auth/decorator';
+import { CreateStokeOperation, EditStokeOperation } from './dto';
 
+@UseGuards(JwtGuard)
 @Controller('stoke-operations')
-export class StokeOperationsController {}
+export class StokeOperationsController {
+  constructor(private stokeOperationsService: StokeOperationsService) { }
+
+  @Get()
+  getStokeOperations(@GetUser('id') userId: number) {
+    return `get all for ${userId}`;
+  }
+
+  @Get(':id')
+  getStokeOperationById(
+    @Param('id') id: string,
+    @GetUser('id') userId: number,
+  ) {
+    return `get ${id} for ${userId}`;
+  }
+
+  @Post()
+  createStokeOperation(
+    @GetUser('id') userId: number,
+    @Body() dto: CreateStokeOperation,
+  ) {
+    return `post ${dto} for ${userId}`;
+  }
+
+  @Patch()
+  editStokeOperation(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) stokeOperationId: number,
+    @Body() dto: EditStokeOperation,
+  ) {
+    return `patch param ${stokeOperationId} with dto ${dto} for ${userId}`;
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete('id')
+  deleteStokeOperation(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) stokeOperationId: number,
+  ) {
+    return `delete param ${stokeOperationId} for ${userId}`;
+  }
+}
