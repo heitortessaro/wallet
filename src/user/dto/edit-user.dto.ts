f718d569623269e014c858597a14d1605e7914f8
Heitor <heitor.j.tessaro@gmail.com>
2023-05-28 09:56:23 -0300

    feat(user): define edit-user dto

---
+++ src/user/dto/edit-user.dto.ts
@@ -0,0 +1,35 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsOptional()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  fistName: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+}
