7ae6e2fc462232018a50662e41a328a184c775b3
Heitor <heitor.j.tessaro@gmail.com>
2023-05-28 12:02:31 -0300

    fix(user): add the option to replace the password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { EditUserDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) { }
 
   async editUser(userId: number, dto: EditUserDto) {
+    let hash: string | undefined;
+    if (dto.password) {
+      hash = await argon.hash(dto.password);
+    }
+
+    const { password, ...dtoLocal } = { ...dto, hash };
+
     const user = await this.prisma.user.update({
       where: {
         id: userId,
       },
       data: {
-        ...dto,
+        ...dtoLocal,
       },
     });
 
