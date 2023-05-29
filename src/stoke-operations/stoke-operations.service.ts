8ee6b5c29445b0f9911387d2899e079c9d851471
Heitor <heitor.j.tessaro@gmail.com>
2023-05-29 06:45:01 -0300

    feat(stoke-operations): define the getAll, getById, and create services dor StokeOperations
    it was not tested. Need to create the patch and delete services, and its tests

--- src/stoke-operations/stoke-operations.service.ts
+++ src/stoke-operations/stoke-operations.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateStokeOperation } from './dto';
 
 @Injectable()
-export class StokeOperationsService {}
+export class StokeOperationsService {
+  constructor(private prisma: PrismaService) { }
+
+  async getStokeOperations(userId: number) {
+    return await this.prisma.stockOperation.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  async getStokeOperationById(userId: number, stokeOperationId: number) {
+    return await this.prisma.stockOperation.findFirst({
+      where: {
+        userId,
+        id: stokeOperationId,
+      },
+    });
+  }
+
+  async createStokeOperation(userId: number, dto: CreateStokeOperation) {
+    const operation = await this.prisma.stockOperation.create({
+      data: {
+        ...dto,
+        userId,
+      },
+    });
+    return operation;
+  }
+}
